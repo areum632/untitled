8e4dc8ddd249ebe382491c90300c5d3ed7f14faf
areum632 <areumlee@koreatech.ac.kr>
2017-07-11 06:44:02 +0900

    Test:Beautifulsoup 네이버웹툰목록2

--- Test.py
+++ Test.py
@@ -7,5 +7,6 @@ soup = BeautifulSoup(html, "html")
 titles = soup.find_all("a", "title")
 
 
+
 for title in titles:
     print 'title:{0:10s} link:{1:20s}‚n'.format(title['title'].encode('utf-8'), title['href'].encode('utf-8'))
